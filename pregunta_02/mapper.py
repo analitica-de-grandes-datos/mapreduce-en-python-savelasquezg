cd20ad4c5970df7d645f52a47d7483b265f4a386
savelasquezg <95106529+savelasquezg@users.noreply.github.com>
2022-06-28 23:35:30 -0500

    Update mapper.py

--- pregunta_02/mapper.py
+++ pregunta_02/mapper.py
@@ -1,3 +1,11 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+def pa(x):
+    return x[3] + "*" + x[4]
+
+for line in sys.stdin:
+    result = line.split(',')
+    print(pa(result))
