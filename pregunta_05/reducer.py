d52f8cb5325b3dc428a0cb72e033b9e3e1602ce8
savelasquezg <95106529+savelasquezg@users.noreply.github.com>
2022-06-29 00:06:14 -0500

    Update reducer.py

--- pregunta_05/reducer.py
+++ pregunta_05/reducer.py
@@ -1,3 +1,18 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+bp = {}
+
+
+def set_bp(dictionary_purposes, actual_element):
+    actual_element = actual_element.replace("\n", "")
+    dictionary_purposes[actual_element] = int(dictionary_purposes.get(actual_element) or 0) + 1 
+    return dictionary_purposes
+
+  
+for line in sys.stdin:
+    set_bp(bp, line)
+
+for purpose, amount in bp.items():
+    print(purpose + "	" + str(amount))
