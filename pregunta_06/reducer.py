53f236d50b50465689918fd758c85c98fc1db0d4
savelasquezg <95106529+savelasquezg@users.noreply.github.com>
2022-06-29 00:23:43 -0500

    Update reducer.py

--- pregunta_06/reducer.py
+++ pregunta_06/reducer.py
@@ -1,3 +1,20 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+ba = {}
+s = {}
+
+def set_bsmalleramount(ba, sa, aelement):
+    earray = aelement.split("*")
+    ba[earray[0]] = max(
+        float(ba.get(earray[0]) or 0), float(earray[1]))
+    sa[earray[0]] = min(
+        float(sa.get(earray[0]) or 10000), float(earray[1]))
+    return ba, samount
+
+for line in sys.stdin:
+    set_bsmalleramount(ba, sa, line)
+
+for max, min in zip(ba.items(), sa.items()):
+    print( max[0] + "	" + str(max[1]) + "	" + str(min[1]) )
