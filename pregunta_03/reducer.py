22d26038e5301894c4cc43aa1cd06cb697b997a9
savelasquezg <95106529+savelasquezg@users.noreply.github.com>
2022-06-28 23:40:16 -0500

    Update reducer.py

--- pregunta_03/reducer.py
+++ pregunta_03/reducer.py
@@ -1,3 +1,10 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+
+for line in sys.stdin:
+    line = line.replace("\n","")
+    if '*' in line:
+        print(line.split("*")[1]+ "," +line.split("*")[0])
