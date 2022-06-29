761ca6088533fb0fe71bdcadfde3639f9c3ecc3d
savelasquezg <95106529+savelasquezg@users.noreply.github.com>
2022-06-28 23:52:41 -0500

    Update reducer.py

--- pregunta_04/reducer.py
+++ pregunta_04/reducer.py
@@ -1,3 +1,17 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+bp = {}
+
+
+def set_bp(dictionary_purposes, actual_element):
+    element_array = actual_element.split("*")
+    dictionary_purposes[element_array[0]] = int(dictionary_purposes.get(actual_element[0]) or 0) + 1 
+    return dictionary_purposes
+
+for line in sys.stdin:
+    set_bp(biggest_purposes, line)
+
+for purpose, amount in bp.items():
+    print(purpose + "," + str(amount))
