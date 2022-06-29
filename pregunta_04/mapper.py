f0447b79282bbb8c39de16e62ab33edbcd91a642
savelasquezg <95106529+savelasquezg@users.noreply.github.com>
2022-06-28 23:44:11 -0500

    Update mapper.py

--- pregunta_04/mapper.py
+++ pregunta_04/mapper.py
@@ -1,3 +1,18 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+def cspaces(x):
+    x = x.replace("\n", "")
+    x = x.replace("\r", "")
+    return x;
+
+  
+def pa(x):
+    return cspaces(x[0])  + "*" + cspaces(x[2])
+
+for line in sys.stdin:
+    line = line.replace("'","")
+    result = line.split('   ')
+    print(pa(result))
