d2733c9d951f701ded4a28a340569e1c46dcfa3b
savelasquezg <95106529+savelasquezg@users.noreply.github.com>
2022-06-29 01:02:35 -0500

    Update reducer.py

--- pregunta_10/reducer.py
+++ pregunta_10/reducer.py
@@ -1,3 +1,25 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+
+import sys
+elements = {}
+def cspaces(x):
+    x = x.replace("\n", "")
+    x = x.replace("\r", "")
+    return x
+
+def set_biggersmalleramount(elements, aelement):
+    earray = aelement.split("*")
+    if element_array[0] in elements:
+        elements[element_array[0]] = elements.get(element_array[0]) +  [int(clear_spaces(element_array[1]))]
+    else:
+        elements[element_array[0]] = [int(clear_spaces(element_array[1]))]
+    return elements
+
+for line in sys.stdin:
+    set_biggersmalleramount(elements, line)
+
+for element, array in  elements.items():
+    array = ','.join(str(v) for v in sorted(array))
+    print(element + "	" + array)
