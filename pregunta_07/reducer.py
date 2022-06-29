de4076bbc32225b3f49632a1875aa244dfc4a170
savelasquezg <95106529+savelasquezg@users.noreply.github.com>
2022-06-29 00:31:52 -0500

    Update reducer.py

--- pregunta_07/reducer.py
+++ pregunta_07/reducer.py
@@ -1,3 +1,31 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+bp = {}
+
+
+def set_bp(dictionary_purposes, aelement):
+    aelement = aelement.replace("\n", "")
+    letter = aelement.split(" ") [0]
+    if  str(dictionary_purposes.get(letter) or "") == "":
+        dictionary_purposes[letter] =    str(dictionary_purposes.get(letter) or "") + actual_element.split(" ") [1]
+    else:
+        dictionary_purposes[letter] =    str(dictionary_purposes.get(letter) or "") +  "*" + actual_element.split(" ") [1]
+    return dictionary_purposes
+
+
+def sortByColumn3(element):
+    return int(element.split(";")[0])
+
+
+for line in sys.stdin:
+    set_bp(bp, line)
+
+
+for purpose, values_and_dates in bp.items():
+    elements = values_and_dates.split("*")
+    elements.sort(key =sortByColumn3)
+    bp[purpose] = elements
+    for element in elements:
+        print(purpose + "   " + element.split(";")[1] + "   " + element.split(";")[0] )
