acde723525ba01bb985d3ee38b8da0b155506242
savelasquezg <95106529+savelasquezg@users.noreply.github.com>
2022-06-28 23:36:47 -0500

    Update reducer.py

--- pregunta_02/reducer.py
+++ pregunta_02/reducer.py
@@ -1,3 +1,17 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+bp = {}
+
+def set_bigger_purpose(dictionary_purposes, actual_element):
+    element_array = actual_element.split("*")
+    dictionary_purposes[element_array[0]] = max(
+        int(dictionary_purposes.get(element_array[0]) or 0), int(element_array[1]))
+    return dictionary_purposes
+
+for line in sys.stdin:
+    set_bp(bp, line)
+
+for purpose, amount in bp.items():
+    print(purpose + "	" + str(amount))
