fc7d8871b3ce0ec214808f30a3586213bc822423
savelasquezg <95106529+savelasquezg@users.noreply.github.com>
2022-06-28 23:39:10 -0500

    Update mapper.py

--- pregunta_03/mapper.py
+++ pregunta_03/mapper.py
@@ -1,3 +1,16 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+def clear_spaces(x):
+    x = x.replace("\n", "")
+    x = x.replace("\r", "")
+    return x
+
+def pa(x):
+    return  clear_spaces(x[1]) +  "*" + clear_spaces(x[0])
+
+for line in sys.stdin:
+    result = line.split(',')
+    print(pa(result))
