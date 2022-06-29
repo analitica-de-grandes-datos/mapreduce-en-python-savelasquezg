bbece8fac23281f4fab66595fb57875e7ec15368
savelasquezg <95106529+savelasquezg@users.noreply.github.com>
2022-06-28 23:26:47 -0500

    Update mapper.py

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -1,3 +1,11 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+def pamount(x):
+    return x[2]
+
+for line in sys.stdin:
+    result = line.split(',')
+    print(pamount(result))
